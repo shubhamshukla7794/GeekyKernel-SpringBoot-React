1f68225ebd64fe0a5415ada65cb5a85b38ed2900
shubhamshukla7794 <shubhamshukla7794@gmail.com>
2024-02-01 15:44:09 +0530

    Initialisation

---
+++ GeekyKernal-Backend/src/main/java/com/shubham/geekykernel/GeekyKernelApplication.java
@@ -0,0 +1,13 @@
+package com.shubham.geekykernel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeekyKernelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeekyKernelApplication.class, args);
+	}
+
+}
---
+++ GeekyKernal-Backend/src/test/java/com/shubham/geekykernel/GeekyKernelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shubham.geekykernel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeekyKernelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
